989f1a293cb2d9410d86162ac314435cc448e3ad
yunusmrkara <104261097+yunusmrkara@users.noreply.github.com>
2022-05-12 08:52:31 +0300

    Hava Durumu sayfası oluşturuldu.

---
+++ hava_durumu/app.js
@@ -0,0 +1,41 @@
+const containerDOM = document.querySelector('.container');
+
+const API_KEY = 'e81faec80cd13bab068b2f4bc08679e9';
+
+const günler = [
+  'Pazar',
+  'Pazartesi',
+  'Salı',
+  'Çarşamba',
+  'Perşembe',
+  'Cuma',
+  'Cumartesi',
+];
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/onecall?lat=40.6940&lon=30.4358&exclude={part}&appid=${API_KEY}&lang=tr`,
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    const days = data.daily;
+    console.log(days);
+    data.daily.forEach((element, index) => {
+      let articleDOM = document.createElement('article');
+      containerDOM.appendChild(articleDOM);
+      articleDOM.classList.add('single-user');
+      articleDOM.innerHTML = `
+      <div class="content-card">
+      <div class="single-item-pic">      
+      <img src="https://openweathermap.org/img/wn/${
+        element.weather[0].icon
+      }@2x.png" >
+      </div><div class="single-item-text"><p>${
+        günler[new Date(element.dt * 1000).getDay()]
+      }</p><p>${
+        element.weather[0].description
+      }</p><p>${Math.round(element.temp['max'] - 273.15)}/
+      ${Math.round(element.temp['min'] - 273.15)}&#8451</p>
+      </div></div>  `;
+    });
+  });
